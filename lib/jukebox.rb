79f1f74dab8f408e3b4eee99685efac5338d8794
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-22 00:23:04 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -41,7 +41,7 @@ def run(songs)
   help
   puts "Please enter a command:"
   user_response = gets.chomp
-  while user_response == "exit"
+  while user_response = "exit"
     case user_response
       when "help"
         help
