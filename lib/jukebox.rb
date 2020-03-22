63c3568d1cdac5be6d79bcf60cd581f4c3e5ce39
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-22 00:19:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,15 +1,1 @@
 require 'pry'
-
-def help
-  puts "I accept the following commands:"
-  puts "- help : displays this help message"
-  puts "- list : displays a list of songs you can play"
-  puts "- play : lets you choose a song to play"
-  puts "- exit : exits the program"
-end
-
-def list(my_songs)
-  my_songs.keys.each do |song|
-    puts song
-  end
-end
