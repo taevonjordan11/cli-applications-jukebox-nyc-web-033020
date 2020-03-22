e8d78251eb03009bdd9b0b4bc0d55eaacd8c930e
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-22 00:20:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,35 +1,65 @@
 require 'pry'
 
+# def help
+#   puts "I accept the following commands:"
+#   puts "- help : displays this help message"
+#   puts "- list : displays a list of songs you can play"
+#   puts "- play : lets you choose a song to play"
+#   puts "- exit : exits this program"
+# end
+
+# def play(songs)
+#   puts "Please enter a song name or number:"
+#   choice = gets.chomp
+#   valid = false
+#   songs.each_with_index do |song, index|
+#     if (choice.to_i - 1 == index)
+#       puts "Playing #{songs[index]}"
+#       valid = true
+#     elsif (choice == song)
+#       puts "Playing #{choice}"
+#       valid = true
+#     end
+#   end
+#   puts "Invalid input, please try again" if valid == false
+# end
+
+# def list(songs)
+#   songs.each_with_index{|song, index| puts "#{index + 1}. #{song}"}
+# end
+
 def help
-  puts "I accept the following commands:\n
-- help : displays this help message\n
-- list : displays a list of songs you can play\n
-- play : lets you choose a song to play\n
-- exit : exits this program"
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
-
 def list(songs)
-  songs.each_with_index do |song,index|
-    puts "#{index + 1}. #{song}"
-  end
+  songs.each_with_index { |song, index|
+    puts (index + 1).to_s + ". " + song
+    }
 end
 
 def play(songs)
-  list(songs)
-  puts
-  puts "What song do you want to play. Enter song name or number"
-  song_pick = gets.chomp.downcase
-  # songs.each_with_index do |song,index|
-    if songs.include?(song_pick) 
-      puts "Now playing song #{song_pick}"
-    elsif songs[song_pick.to_i - 1] 
-      puts "Now playing #{songs[song_pick.to_i - 1]}"
-    else
-      puts "Invalid input. Please try again!"
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp
+  output = ""
+  songs.each_with_index { |song, index|
+    if user_response == (index + 1).to_s || user_response == song
+      output = "Playing #{song}"
     end
+  }
+  if output.include?("Playing")
+    puts output
+  elsif user_response == "list"
+    list(songs)
+    play(songs)
+  else
+    puts "Invalid input, please try again"
+  end
 end
-
 
 def exit_jukebox
   puts "Goodbye"
@@ -37,24 +67,26 @@ end
 
 def run(songs)
   help
-  puts  
-  puts "Please make a selection"
-  choice = gets.chomp
-  until choice == 'exit'
-    case choice
-
-    when 'help'
-      help
-    when 'list'
-      list(songs)
-    when 'play'
-      play(songs)
-    else
-      puts 'That is not a option! Try Again.'
+  puts "Please enter a command:"
+  user_response = gets.chomp
+  while user_response != "exit"
+    case user_response
+      when "help"
+        help
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "list"
+        list(songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "play"
+        play(songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      else
+        puts "Invalid input, please try again"
+        user_response = gets.chomp
     end
-  puts
-  puts "Please make a selection"
-  choice = gets.chomp
   end
-exit_jukebox
-end
+  exit_jukebox
+end 
