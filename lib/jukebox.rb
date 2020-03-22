4940a690a673ef65f0fb6240b8968e7555dd1d85
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-22 00:31:01 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,59 @@
-# Add your code here
+require 'pry'
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index { |song, index|
+    puts (index + 1).to_s + ". " + song
+    }
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_response = gets.chomp
+  output = ""
+  songs.each_with_index { |song, index|
+    if user_response == (index + 1).to_s || user_response == song
+      output = "Playing #{song}"
+    end
+  }
+  if output.include?("Playing")
+    puts output
+  elsif user_response == "list"
+    list(songs)
+    play(songs)
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  prompt = "Please enter a command:"
+  puts prompt
+    while command = gets.chomp
+    case command 
+    when "list"
+      list(songs)
+      puts prompt
+    when "play"
+      play(songs)
+      puts prompt
+    when "help"
+      help
+      puts prompt
+    when "exit"
+      exit_jukebox
+      break
+    end
+  end
+end
